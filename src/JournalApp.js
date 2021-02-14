66cd0cd30558737301901acfc3edbed7015daf8e
Jorge Barcos <jorgebarcos88@gmail.com>
2021-02-13 22:05:39 -0300

    Implementando rutas principales y rutas hijas

--- src/JournalApp.js
+++ src/JournalApp.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./styles/styles.scss";
+import { AppRouter } from "./routers/AppRouter";
 
 export const JournalApp = () => {
   return (
-    <div>
-      <h1>Journal App</h1>
-    </div>
+    <>
+      <AppRouter />
+    </>
   );
 };
