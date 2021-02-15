9b44b0d36ec42e93012a2b37218f642a2e1598e7
Jorge Barcos <jorgebarcos88@gmail.com>
2021-02-15 12:04:04 -0300

    Sidebar - JournalEntries

---
+++ src/components/journal/JournalEntry.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+export const JournalEntry = () => {
+  return (
+    <div className="journal__entry pointer">
+      <div
+        className="journal__entry-picture"
+        style={{
+          backgroundSize: "cover",
+          backgroundImage:
+            "url(https://conceptodefinicion.de/wp-content/uploads/2014/05/imagen.jpg)",
+        }}
+      ></div>
+      <div className="journal__entry-body">
+        <p className="journal__entry-title">Un nuevo día</p>
+        <p className="journal__entry-content">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+        </p>
+      </div>
+      <div className="journal__entry-date-box">
+        <span>Monday</span>
+        <h4>28</h4>
+      </div>
+    </div>
+  );
+};
--- src/components/journal/Sidebar.js
+++ src/components/journal/Sidebar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { JournalEntries } from "./JournalEntries";
 
 export const Sidebar = () => {
   return (
@@ -15,6 +16,8 @@ export const Sidebar = () => {
         <i className="far fa-calendar-plus fa-5x"></i>
         <p className="mt-5">New entry</p>
       </div>
+
+      <JournalEntries />
     </aside>
   );
 };
