470a4af312a2219a81eaf8cbb82b63c1fec98b34
Jorge Barcos <jorgebarcos88@gmail.com>
2021-07-13 14:08:35 -0300

    Mostrar las notas en el menú lateral

--- src/components/journal/JournalEntry.js
+++ src/components/journal/JournalEntry.js
@@ -1,25 +1,30 @@
 import React from "react";
+import moment from 'moment'
 
-export const JournalEntry = () => {
+export const JournalEntry = ({ id, date, title, body, url }) => {
+
+  const noteDate = moment(date)
+
   return (
     <div className="journal__entry pointer">
-      <div
-        className="journal__entry-picture"
-        style={{
-          backgroundSize: "cover",
-          backgroundImage:
-            "url(https://conceptodefinicion.de/wp-content/uploads/2014/05/imagen.jpg)",
-        }}
-      ></div>
+      {url &&
+        <div
+          className="journal__entry-picture"
+          style={{
+            backgroundSize: "cover",
+            backgroundImage:
+              `url(${url})`,
+          }}
+        ></div>}
       <div className="journal__entry-body">
-        <p className="journal__entry-title">Un nuevo día</p>
+        <p className="journal__entry-title">{title}</p>
         <p className="journal__entry-content">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+          {body}
         </p>
       </div>
       <div className="journal__entry-date-box">
-        <span>Monday</span>
-        <h4>28</h4>
+        <span>{noteDate.format('dddd')}</span>
+        <h4>{noteDate.format('Do')}</h4>
       </div>
     </div>
   );
