eb6db07f8b4fe3c05682051bbf7e4f76207cdac0
Jorge Barcos <jorgebarcos88@gmail.com>
2021-07-13 10:56:44 -0300

    Cargar notas de Firestore

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -11,6 +11,8 @@ import { firebase } from '../firebase/firebase-config'
 import { login } from "../actions/auth";
 import { PrivateRoute } from "./PrivateRoute";
 import { PublicRoute } from "./PublicRoute";
+import { loadNotes } from "../helpers/loadNotes";
+import { setNotes } from "../actions/notes";
 
 export const AppRouter = () => {
 
@@ -22,10 +24,12 @@ export const AppRouter = () => {
 
   useEffect(() => {
 
-    firebase.auth().onAuthStateChanged((user) => {
+    firebase.auth().onAuthStateChanged(async (user) => {
       if (user?.uid) {
         dispatch(login(user.uid, user.displayName))
         setIsLoggedIn(true)
+        const notes = await loadNotes(user.uid)
+        dispatch(setNotes(notes))
       } else {
         setIsLoggedIn(false)
       }
