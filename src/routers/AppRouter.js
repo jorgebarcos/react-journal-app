b99190b4bd7d3ec3152db7cd6d5d3261d63079ae
Jorge Barcos <jorgebarcos88@gmail.com>
2021-07-13 13:53:52 -0300

    Optimización de la petición

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -11,8 +11,8 @@ import { firebase } from '../firebase/firebase-config'
 import { login } from "../actions/auth";
 import { PrivateRoute } from "./PrivateRoute";
 import { PublicRoute } from "./PublicRoute";
-import { loadNotes } from "../helpers/loadNotes";
-import { setNotes } from "../actions/notes";
+
+import { startLoadingNotes } from "../actions/notes";
 
 export const AppRouter = () => {
 
@@ -28,8 +28,8 @@ export const AppRouter = () => {
       if (user?.uid) {
         dispatch(login(user.uid, user.displayName))
         setIsLoggedIn(true)
-        const notes = await loadNotes(user.uid)
-        dispatch(setNotes(notes))
+
+        dispatch(startLoadingNotes(user.uid))
       } else {
         setIsLoggedIn(false)
       }
